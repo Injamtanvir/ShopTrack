06215eab47c907e4e93088a09e3d222c856874aa
INJAM UL HAQUE <injamtanvir@gmail.com>
2025-04-02 18:23:48 +0600

    Frontend Update2

---
+++ frontend/shoptrack/android/app/src/main/kotlin/gnvox/example/shoptrack/MainActivity.kt
@@ -0,0 +1,5 @@
+package gnvox.example.shoptrack
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ frontend/shoptrack/lib/constants/api_constants.dart
@@ -0,0 +1,15 @@
+class ApiConstants {
+  // Base URL - Production Render URL
+  static const String baseUrl = 'https://api.render.com/deploy/srv-cvmgifffte5s739i07eg?key=CZYEy-bGCFI';
+
+  // For local development (comment out when using production)
+  // static const String baseUrl = 'http://10.0.2.2:8000/api'; // For Android emulator
+  // static const String baseUrl = 'http://localhost:8000/api'; // For web/iOS
+
+  // Endpoints
+  static const String registerShop = '$baseUrl/register-shop/';
+  static const String login = '$baseUrl/login/';
+  static const String registerSalesPerson = '$baseUrl/register-sales-person/';
+  static const String registerAdmin = '$baseUrl/register-admin/';
+  static const String verifyToken = '$baseUrl/verify-token/';
+}
