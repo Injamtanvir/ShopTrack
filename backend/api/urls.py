10236fe09b2bc59416056e9e5d11b9f0973b414a
INJAM UL HAQUE <injamtanvir@gmail.com>
2025-04-03 21:21:42 +0600

    koala.v6

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .db import NextInvoiceNumberView  # Add this import
 
 urlpatterns = [
     path('register-shop/', views.ShopRegistrationView.as_view(), name='register-shop'),
@@ -18,4 +19,6 @@ urlpatterns = [
     path('invoice-history/<str:shop_id>/', views.InvoiceHistoryView.as_view(), name='invoice-history'),
     path('invoice/<str:invoice_id>/', views.InvoiceDetailView.as_view(), name='invoice-detail'),
     path('search-products/<str:shop_id>/', views.SearchProductsView.as_view(), name='search-products'),
+
+    path('next-invoice-number/<str:shop_id>/', NextInvoiceNumberView.as_view(), name='next-invoice-number'),
 ]
